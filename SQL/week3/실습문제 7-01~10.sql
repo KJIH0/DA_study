e57f16ed8bc04db40918cb6f3728c4938c298ce6
Kim Jiho <83413923+KJIH0@users.noreply.github.com>
2022-01-08 23:55:49 +0900

    WEEK 3 DAY 2-4

--- SQL/week3/실습문제 7-01~10.sql
+++ SQL/week3/실습문제 7-01~10.sql
@@ -14,7 +14,14 @@ GROUP BY ROLLUP (store_id)
 
 --문제3번) 국가(country)별 도시(city)별 매출액, 국가(country)매출액 소계 그리고 전체 매출액을 구하세요. (grouping set)
 
-
+SELECT c3.country, c2.city, sum(p.amount)
+  FROM payment p 
+JOIN customer c USING (customer_id)
+JOIN address a USING (address_id)
+JOIN city c2 USING (city_id)
+JOIN country c3 USING (country_id)
+GROUP BY GROUPING SETS (country, city), (country), ()
+ORDER BY country
 
 --문제4번) 국가(country)별 도시(city)별 매출액, 국가(country)매출액 소계 그리고 전체 매출액을 구하세요. (rollup)
 
