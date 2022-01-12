5d17c857faedf2e009ae17105aab417375a28b6c
Kim Jiho <83413923+KJIH0@users.noreply.github.com>
2022-01-12 23:10:20 +0900

    WEEK 3 DAY 6
    7번문제 이해 못함

---
+++ SQL/week3/실습문제 7-07~10.sql
@@ -0,0 +1,38 @@
+--문제7번) 영화에서 사용한 언어와  영화 개봉 연도 에 대한 영화  갯수와, 영화 개봉 연도에 대한 영화 갯수를 함께 보여주세요.
+-- 뭔소린지 모르겠음
+
+
+
+--문제8번) 연도별, 일별 결제  수량과,  연도별 결제 수량을 함께 보여주세요.
+-- 결제수량은 결제 의 id 갯수 를 의미합니다.
+
+SELECT to_char(payment_date, 'YYYY') AS "Year", 
+       COALESCE (to_char(payment_date, 'MM-DD'), 'Total') AS "Date", 
+       count(payment_id)
+  FROM payment
+GROUP BY 
+GROUPING SETS (to_char(payment_date, 'YYYY'), to_char(payment_date, 'MM-DD')), (to_char(payment_date, 'YYYY'))
+ORDER BY "Date"
+
+
+--문제9번) 지점 별,  active 고객의 수와 ,   active 고객 수 를  함께 보여주세요.
+-- 지점과, active 여부에 대해서는 customer 테이블을 이용하여 보여주세요.
+-- grouping sets 를 이용해서 풀이해주세요.
+-- COALESCE를 이렇게도 쓸 수 있구나! 태규님 답안을 참조했다. 신세계,,, 총합이 null로 처리되는게 보기 싫었는데 해결방법을 찾았다.
+-- 그 외 COALESCE 문법은 https://stackoverflow.com/questions/20181422/invalid-input-syntax-for-type-numeric-for-entering-emptyness 참조하여 공부함
+
+SELECT COALESCE (store_id::TEXT, 'Total') AS "store", active, count(customer_id) AS actovie_user_count
+  FROM customer c 
+GROUP  BY grouping sets( ( store_id, active ), ( active ) )
+ORDER BY store_id
+
+
+--문제10번) 지점 별,  active 고객의 수와 ,   active 고객 수 를  함께 보여주세요.
+-- 지점과, active 여부에 대해서는 customer 테이블을 이용하여 보여주세요.
+-- roll up으로 풀이해보면서, grouping sets 과의 차이를 확인해보세요.
+
+SELECT COALESCE (store_id::TEXT , 'Total') AS "store_id", active, Count(*) 
+  FROM customer
+LEFT JOIN store s USING (store_id)
+GROUP BY ROLLUP (store_id), (active)
+ORDER BY store_id
