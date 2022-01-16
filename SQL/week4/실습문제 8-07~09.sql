eb6bc89ab801e51d7490ea491d4637c6956dd2ad
Kim Jiho <83413923+KJIH0@users.noreply.github.com>
2022-01-16 18:46:22 +0900

    WEEK 4 DAY 4

--- SQL/week4/실습문제 8-07~09.sql
+++ SQL/week4/실습문제 8-07~09.sql
@@ -33,10 +33,10 @@ SELECT t1.*
 
 SELECT t1.*
  FROM (SELECT c.name, 
-			        f.title, 
+       	      f.title, 
               count(r.rental_id) AS cnt,
               ROW_NUMBER () OVER (PARTITION BY c.name ORDER BY count(r.rental_id) DESC, title) AS "CATEGORY_RANK"
-		   FROM category c
+       FROM category c
        JOIN film_category fc ON c.category_id = fc.category_id
        JOIN inventory i ON fc.film_id = i.film_id 
        JOIN rental r ON i.inventory_id = r.inventory_id 
